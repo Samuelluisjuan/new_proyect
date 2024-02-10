316b297ff832fffb5e96bbccc9ef01ea1979f7a0
Samuel Luisj <samuelluisjuan1@gmail.com>
2024-02-09 20:44:24 -0600

    intento deo nuevo commid

--- app.py
+++ app.py
@@ -2,16 +2,34 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-title = st.header('Cuadro estadistico de Veiculos vendidos')
-        
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+car_data = pd.read_csv('vehicles_us.csv')  # Leer los datos
+
+# Crear un título para la aplicación
+st.title('Análisis de vehículos vendidos')
+
+show_hist = st.checkbox('Mostrar histograma') # Casillas de verificación para visualizaciones
+show_scatter = st.checkbox('Mostrar scatterplot') # Casillas de verificación para visualizaciones
+
+# Si se selecciona la casilla de verificación para el histograma
+if show_hist:
+    # Crear un título para el histograma
+    st.header('Histograma de odómetros')
+    
+    # Crear un histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+    
+    # Mostrar el histograma
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Si se selecciona la casilla de verificación para el scatterplot
+if show_scatter:
+    # Crear un título para el scatterplot
+    st.header('Gráfico de dispersión entre año de fabricación y precio')
+    
+    # Crear el scatterplot
+    fig_scatter = px.scatter(car_data, x="year", y="price", 
+                             title="Precio vs. Año de Fabricación",
+                             labels={"year": "Año de Fabricación", "price": "Precio"})
+    
+    # Mostrar el scatterplot
+    st.plotly_chart(fig_scatter, use_container_width=True)
